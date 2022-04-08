2fd9762ea10ada6271c54cff1e24118c777b8dfe
PeasantCoding <986700329@qq.com>
2022-04-08 17:23:10 +0800

    商品信息导入ES，商品搜索功能

---
+++ ordermeal-parent/ordermeal-service/ordermeal-service-search/src/main/java/com/ordermeal/search/dao/SkuEsMapper.java
@@ -0,0 +1,10 @@
+package com.ordermeal.search.dao;
+
+import com.ordermeal.search.pojo.SkuInfo;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface SkuEsMapper extends ElasticsearchRepository<SkuInfo,Long> {
+}
